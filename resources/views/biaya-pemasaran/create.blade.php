aea9f092fdb39afa707f3c1becf1b4d54cdd5054
refrita <231210072@student.mercubuana-yogya.ac.id>
2025-05-16 12:24:30 +0700

    Update perubahan

--- resources/views/biaya-pemasaran/create.blade.php
+++ resources/views/biaya-pemasaran/create.blade.php
@@ -14,4 +14,18 @@
         </div>
         <div class="mb-3">
             <label class="form-label">Anggaran Tersedia</label>
-            <
+            <input type="number" name="anggaran_tersedia" class="form-control">
+        </div>
+        <div class="mb-3">
+            <label class="form-label">Bulan Berlaku</label>
+            <input type="date" name="bulan_berlaku" class="form-control">
+        </div>
+        <div class="mb-3">
+            <label class="form-label">Status</label>
+            <input type="text" name="status" class="form-control">
+        </div>
+        <button type="submit" class="btn btn-primary">Simpan</button>
+        <a href="{{ route('biaya-pemasaran.index') }}" class="btn btn-secondary ms-2">← Kembali</a>
+    </form>
+</div>
+@endsection
